10975995cae7a5bedd9cc2679efc02773b0062f5
Ali <salavati.cuber@gmail.com>
2025-11-06 18:36:11 +0330

    AbstractResource

--- src/Resources/AbstractResource.php
+++ src/Resources/AbstractResource.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Sapak\Sms\Resources;
+
+use GuzzleHttp\ClientInterface;
+
+/**
+ * Base class for all API resources.
+ * Holds the authenticated HTTP client.
+ */
+abstract class AbstractResource
+{
+    public function __construct(
+        protected ClientInterface $httpClient
+    ) {}
+}
