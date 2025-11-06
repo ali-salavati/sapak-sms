0b0472ba8e459bfed6e5c15e3bdae36dd7b5499c
Ali <salavati.cuber@gmail.com>
2025-11-07 01:44:15 +0330

    AccountResource

--- src/Resources/AccountResource.php
+++ src/Resources/AccountResource.php
@@ -1,1 +1,40 @@
 <?php
+
+namespace Sapak\Sms\Resources;
+
+use Sapak\Sms\DTOs\Responses\AccountCredit;
+use Sapak\Sms\Exceptions\ApiException;
+use Sapak\Sms\Exceptions\AuthenticationException;
+use Sapak\Sms\Exceptions\ValidationException;
+
+/**
+ * Handles API interactions related to the user account (e.g., credit).
+ */
+class AccountResource extends AbstractResource
+{
+    /**
+     * Retrieve the current account credit balance.
+     *
+     * @return AccountCredit DTO containing the credit amount.
+     *
+     * @throws ApiException
+     * @throws AuthenticationException
+     * @throws ValidationException
+     */
+    public function getCredit(): AccountCredit
+    {
+        $response = $this->request('get', 'users/me/credit');
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        if (!isset($data['credit']) || !is_numeric($data['credit'])) {
+            throw new ApiException(
+                'Invalid response format from credit endpoint. "credit" key missing or not numeric.'
+            );
+        }
+
+        return new AccountCredit(
+            credit: (float) $data['credit']
+        );
+    }
+}
