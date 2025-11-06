a324b88a5670a375aa5dceaf80644f36ac4edd92
Ali <salavati.cuber@gmail.com>
2025-11-07 01:42:26 +0330

    AccountCredit

--- src/DTOs/Responses/AccountCredit.php
+++ src/DTOs/Responses/AccountCredit.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace Sapak\Sms\DTOs\Responses;
+
+/**
+ * DTO for the "Get Account Credit" response.
+ *
+ * Encapsulates the response from the API, ensuring a strongly-typed result.
+ */
+class AccountCredit
+{
+    /**
+     * @param float $credit The account credit balance (API type: double).
+     */
+    public function __construct(
+        public readonly float $credit
+    ) {}
+}
